b34cb7fd7de8ed68f58e2f9eef46c659c7f7e0c1
ilrudie <ilrudie@gmail.com>
2023-01-24 23:08:56 -0500

    improved output

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 // sample taken directly from jwt.io
@@ -24,30 +25,55 @@ func main() {
 	fmt.Println(s)
 }
 
+type jsonData map[string]interface{}
+
 func decodeJwt(s string) (string, error) {
 
 	// header, payload, sig
 	hps := strings.Split(s, ".")
 
 	// eventual return
-	var decoded string
-	for _, elem := range hps {
+	decoded := fmt.Sprintln("") // start with a newline
+
+	for i, elem := range hps {
+
+		var jwtSection string
+
+		switch i {
+		case 0:
+			jwtSection = "Header"
+		case 1:
+			jwtSection = "Payload"
+		default:
+			jwtSection = "Unknown"
+		}
 
 		// decode the element into a []byte, do nothing if it blows up :)
 		bytes, _ := base64.RawURLEncoding.DecodeString(elem)
 
 		// declare some generic housing for json
-		var jsonOutput map[string]interface{}
+		var unmarshalledData jsonData
 
 		// try to unmarshal, just move on if it blows up
-		err := json.Unmarshal(bytes, &jsonOutput)
+		err := json.Unmarshal(bytes, &unmarshalledData)
 		if err != nil {
 			// just skip this one, I guess it's bad
 			continue
 		}
 
-		// rudamentary append onto the return
-		decoded += fmt.Sprintf("%v\n", jsonOutput)
+		for k, v := range unmarshalledData {
+
+			// TODO: this only works at the top level, technically a JWT could contain a nested JWT so consider handling that
+			numericDate, ok := v.(float64)
+			if ok { // NumericDate is the format for timestamps, golang reads it as a float64 so we can detect timestamps and format them better
+				unmarshalledData[k] = time.Unix(int64(numericDate), 0).Format(time.RFC1123)
+			}
+
+		}
+
+		formattedBytes, _ := json.MarshalIndent(unmarshalledData, "", "    ")
+
+		decoded += fmt.Sprintf("%s:\n%s\n", jwtSection, string(formattedBytes))
 
 	}
 
