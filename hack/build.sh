e53145c979e98080085395c4f1ff04d9fc9dbc19
ilrudie <ilrudie@gmail.com>
2023-02-22 20:29:35 -0500

    upgrade to chainguard images and full static build

--- hack/build.sh
+++ hack/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # build basic golang server
-go build -o ./server ./cmd/server/.
+CGO_ENABLED=0 go build -o ./server ./cmd/server/.
 
 # build the Web Assebly binary we'll serve
 GOOS=js GOARCH=wasm go build -o ./jwt.wasm ./cmd/wasm/.
